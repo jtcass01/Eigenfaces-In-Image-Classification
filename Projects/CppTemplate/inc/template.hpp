d2f4278f16965760440f32270ab68de800d5afed
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2024-05-03 10:19:34 -0400

    Initial commit

---
+++ Projects/CppTemplate/inc/template.hpp
@@ -0,0 +1,13 @@
+#ifndef __TEMPLATE_HPP__
+#define __TEMPLATE_HPP__
+
+class Example
+{
+public:
+    Example();
+    ~Example();
+};
+
+int ExampleFunction(int a, int b);
+
+#endif // __TEMPLATE_HPP__
---
+++ Projects/CppTemplate/src/template.cpp
@@ -0,0 +1,16 @@
+#include "../inc/template.hpp"
+
+Example::Example()
+{
+    // Constructor
+}
+
+Example::~Example()
+{
+    // Destructor
+}
+
+int ExampleFunction(int a, int b)
+{
+    return a + b;
+}
